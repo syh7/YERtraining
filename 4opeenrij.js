ddf1bffbc71eb6af8e0dbec23c34bd335bddb5b0
Student <sjoerd0707@live.nl>
2019-10-14 16:29:53 +0200

    Change column colour when hovering

--- 4opeenrij.js
+++ 4opeenrij.js
@@ -4,6 +4,9 @@ const COLS = 7;
 
 let boardDiv = document.getElementById("boarddiv");
 let boardTable = document.getElementById("boardtable");
+let rowArray = [];
+let clickedCell;
+let hoveredCell;
 
 function makeBoard(){
 	for(let i = 0; i < ROWS; i++){
@@ -11,15 +14,50 @@ function makeBoard(){
 		for(let j = 0; j < COLS; j++){
 			let td = document.createElement("td");
 			td.classList.add("empty");
+			td.addEventListener('click', function(){
+				clickCell(this);
+			});
+			td.addEventListener('mouseover', function(){
+				mouseOverCell(this)
+			});
 			tr.appendChild(td);
 		}
 		boardTable.appendChild(tr);
+		rowArray.push(tr);
 	}
 }
 
 
-function clickColumn(col){
-	console.log("function: clickColumn");
+
+function clickCell(cell){
+	console.log(cell);
+	clickedCell = cell;
+	
+}
+
+function mouseOverCell(cell){
+	console.log("hovering");
+	hoveredCell = cell;
+	let index = -1;
+	
+	//get column number in row
+	for(let i = 0; i < cell.parentElement.childElementCount; i++){
+		if(cell === cell.parentElement.children[i]){
+			index = i;
+			break;
+		}
+	}
+
+	for(let i = 0; i < ROWS; i++){
+		for(let j = 0; j < COLS; j++){
+			rowArray[i].children[j].style.backgroundColor = "white";
+		}
+	}
+
+	for(let i = 0; i < ROWS; i++){
+		rowArray[i].children[index].style.backgroundColor = "gray";
+	}
+
 }
 
 function gameFinished(){
