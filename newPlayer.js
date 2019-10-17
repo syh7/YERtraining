931b18a8df6fee62edd4e82a0d70286c4dac5bc4
Student <sjoerd0707@live.nl>
2019-10-17 11:14:03 +0200

    Added empty name check

--- newPlayer.js
+++ newPlayer.js
@@ -11,13 +11,24 @@ function saveNewPlayer(){
     }
 
     let player = {};
-    player.firstName = firstNameInput.value;
-    player.lastName = lastNameInput.value;
-    player.dateOfBirth = dateOfBirthInput.value;
-    player.playerLevel = playerLevelInput.value;
+    if(firstNameInput.value == ""){
+        alert("First name cannot be empty.");
+        return;
+    } else if(lastNameInput.value == ""){
+        alert("Last name cannot be empty.");
+        return;
+    } else if(dateOfBirthInput.value == ""){
+        alert("Date of birth must be entered.");
+        return;
+    } else {
+        player.firstName = firstNameInput.value;
+        player.lastName = lastNameInput.value;
+        player.dateOfBirth = dateOfBirthInput.value;
+        player.playerLevel = playerLevelInput.value;
 
-    players.push(JSON.stringify(player));
-    localStorage.setItem("players", players);
+        players.push(JSON.stringify(player));
+        localStorage.setItem("players", players);
 
-    console.log(players);
+        console.log(players);
+    }
 }
